cead11d16a08e96559dc6f7da6e5ba94a5e06999
Kristina Mickaela Fujiwara <98295543+kristinafujiwara@users.noreply.github.com>
2023-01-04 16:42:56 +0800

    Update Result.java

--- src/Result.java
+++ src/Result.java
@@ -1,6 +1,7 @@
 import javax.swing.*;
 class Result {
-    public static void displayMessage(String answer){
-        JOptionPane.showMessageDialog( null, "WHOLESALE PRICE: " + answer);
+    public static void displayMessage(String retail, String wholesale) {
+        JOptionPane.showMessageDialog(null, "Retail Price is " + retail);
+        JOptionPane.showMessageDialog(null, "Wholesale Price is " + wholesale);
     }
 }
